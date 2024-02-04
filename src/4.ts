6a29e2e414d8c26fcf87db2f05bd2f516c9d2654
VasylPopaz <pafos1997@gmail.com>
2024-02-04 22:07:41 +0100

    fixed tasks

--- src/4.ts
+++ src/4.ts
@@ -1,25 +1,25 @@
 class Key {
   private signature: number = Number((Math.random() * 100).toFixed());
 
-  getSignature() {
+  getSignature(): number {
     return this.signature;
   }
 }
 
 class Person {
   constructor(private key: Key) {}
-  getKey() {
+  getKey(): Key {
     return this.key;
   }
 }
 
 abstract class House {
-  protected door: true | false = false;
-  private tenants: object[] = [];
+  protected door: boolean = false;
+  private tenants: Person[] = [];
 
-  constructor(private key: Key) {}
+  constructor(protected key: Key) {}
 
-  comeIn(person: Person) {
+  comeIn(person: Person): void {
     if (this.door) this.tenants.push(person);
   }
 
@@ -28,7 +28,7 @@ abstract class House {
 
 class MyHouse extends House {
   public openDoor(key: Key): void {
-    if (key === person.getKey()) this.door = true;
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
   }
 }
 const key = new Key();
