fe407bf5ac831859936b433329c3e4ef4590a276
VasylPopaz <pafos1997@gmail.com>
2024-02-04 13:37:50 +0100

    tasks done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,45 @@
+class Key {
+  private signature: number = Number((Math.random() * 100).toFixed());
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: true | false = false;
+  private tenants: object[] = [];
+
+  constructor(private key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) this.tenants.push(person);
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key === person.getKey()) this.door = true;
+  }
+}
 const key = new Key();
-
-const house = new MyHouse(key);
+console.log(key);
 const person = new Person(key);
+const house = new MyHouse(key);
 
+console.log(house);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
+console.log(house);
 
 export {};
